64da1eca83eb45d52ff9960424b66f1a5b41afc9
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2022-12-05 08:32:25 +0300

    if name = main

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
-argc = ((len(argv))-1)
-print(argc, end='')
-if argc == 0:
-    print(' arguments.')
-elif argc == 1:
-    print(' argument:')
-else:
-    print(' arguments:')
-for cnt, arg in enumerate(argv):
-    if cnt == 0:
-        continue
-    else:
-        print(f'{str(cnt).rstrip()}: {arg}')
+if __name__ == '__main__'
+	argc = ((len(argv))-1)
+	print(argc, end='')
+	if argc == 0:
+		print(' arguments.')
+	elif argc == 1:
+		print(' argument:')
+	else:
+		print(' arguments:')
+	for cnt, arg in enumerate(argv):
+		if cnt == 0:
+			continue
+		else:
+			print(f'{str(cnt).rstrip()}: {arg}')
