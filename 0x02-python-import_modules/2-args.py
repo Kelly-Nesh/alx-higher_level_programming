0f97d31d317d422cb142ac93a65a2c059aec7d87
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2022-12-29 17:10:43 +0300

    chmoded

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from sys import argv
-argc = len(argv)-1
-if argc == 0:
-    print("{} arguments.".format(argc))
-elif argc == 1:
-    print("{} argument:\n{}: {}".format(argc, argc, argv[argc]))
-else:
-    print("{} arguments:".format(argc))
-    for arg in range(1, argc+1):
-        print("{}: {}".format(arg, argv[arg]))
+def main():
+    argc = len(argv)-1
+    if argc == 0:
+        print("{} arguments.".format(argc))
+    elif argc == 1:
+        print("{} argument:\n{}: {}".format(argc, argc, argv[argc]))
+    else:
+        print("{} arguments:".format(argc))
+        for arg in range(1, argc+1):
+            print("{}: {}".format(arg, argv[arg]))
+if __name__ == "__main__":
+    main()
