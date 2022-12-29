4ea3673b1a42987893d46e162a6f730f71e652df
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2022-12-29 14:53:11 +0300

    pycodestyle validation violation violine :)

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    s2=''
-    for s,j in enumerate(str):
+    s2 = ''
+    for s, j in enumerate(str):
         if s == n:
             continue
         s2 += j
