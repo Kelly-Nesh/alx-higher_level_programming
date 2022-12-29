72a005fb1b4eaee5cf38db9856ca4b74ed7527dc
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2022-12-29 18:09:19 +0300

    calc.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
+def main():
     from sys import argv, exit
     from calculator_1 import add, sub, mul, div
     argc = len(argv) - 1
     if argc != 3:
-        print("Usage: {} <a> <operator> <b>\n{}".format(argv[0], argc))
+        print("Usage: {} <a> <operator> <b>".format(argv[0]))
         exit(1)
     a = int(argv[1])
     op = argv[2]
@@ -22,3 +22,7 @@ if __name__ == "__main__":
         print("Unkown operator. Available operators: {}, {}, {} and {}"
               .format(ops[0], ops[1], ops[2], ops[3]))
         exit(1)
+
+
+if __name__ == "__main__":
+    main()
