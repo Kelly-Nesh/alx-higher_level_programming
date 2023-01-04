7399ffffa1f8d3cbff5d7704af9f74153b9ca1d5
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-01-04 17:40:06 +0300

    2-rectangle.py change to value

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -20,15 +20,15 @@ class Rectangle:
         return self.__width
 
     @width.setter
-    def width(self, width):
+    def width(self, value):
         """Set the width of the current rectangle
             depending on type and value.
         """
-        if not isinstance(width, (int)):
+        if not isinstance(value, (int)):
             raise TypeError('width must be an integer')
-        elif width < 0:
+        elif value < 0:
             raise ValueError('width must be >= 0')
-        self.__width = width
+        self.__width = value
 
     @property
     def height(self):
@@ -36,15 +36,15 @@ class Rectangle:
         return self.__height
 
     @height.setter
-    def height(self, height):
+    def height(self, value):
         """Set the height of the current rectangle
             depending on type and value.
         """
-        if not isinstance(height, (int)):
+        if not isinstance(value, (int)):
             raise TypeError('height must be an integer')
-        elif height < 0:
+        elif value < 0:
             raise ValueError('height must be >= 0')
-        self.__height = height
+        self.__height = value
 
     def area(self):
         """Returns rectangle area."""
