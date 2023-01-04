24790153c7eabdc504ec866269eaa7af3f8bc8d5
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-01-04 16:09:22 +0300

    1-rectangle spacing redo

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class"""
 
 
 class Rectangle:
-    """A rectangle class representation."""
-
+    """A rectangle representation."""
     def __init__(self, width=0, height=0):
         """Initialize rectangle properties.
 
