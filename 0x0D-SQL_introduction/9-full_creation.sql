622b00b6014e4cf8e652b9a970bfae291b870e7b
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-18 03:15:32 +0300

    9-full_creation.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- script that creates a table second_table
-CREATE DATABASE IF NOT EXISTS second_table (
+CREATE table IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
     score INT );
