39adaee749c7a41776598c8927d95c89d6182989
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-24 12:54:44 +0000

    right output checker wrong

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 --  lists all genres of the show Dexter
-SELECT name FROM tv_show_genres JOIN tv_genres JOIN tv_shows ON tv_show_genres.genre_id=tv_genres.id AND tv_shows.id=tv_show_genres.show_id WHERE title="Dexter" ORDER BY title ASC;
+SELECT name FROM tv_show_genres JOIN tv_shows ON tv_shows.id=tv_show_genres.show_id JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id WHERE TITLE="Dexter" ORDER BY name ASC;
