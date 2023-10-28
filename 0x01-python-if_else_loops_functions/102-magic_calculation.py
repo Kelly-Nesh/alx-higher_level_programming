57addac5c1f74bcb2ebb026fea0519952e11e37e
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-28 16:44:15 +0300

    README.md

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# Python function the same as the given Python bytecode
+
+
+def magic_calculation(a, b, c):
+    """function the same as the given Python bytecode"""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
