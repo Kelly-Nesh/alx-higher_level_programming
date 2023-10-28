7aec033d710f89696b3a569e7ee0333c003bb5c8
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-28 17:21:58 +0300

    added blank line at end

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -7,7 +7,6 @@ def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for col in range(len(matrix[row])):
             print("{:d}".format(matrix[row][col]), end="")
-            if col == len(matrix[row]) - 1:
-                print()
-            else:
+            if col != len(matrix[row]) - 1:
                 print(" ", end="")
+        print()
