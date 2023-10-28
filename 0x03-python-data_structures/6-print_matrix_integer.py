a0fed61dfc8fccea865c527041929b2b2fff61bf
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-28 17:12:53 +0300

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bn/python3
+"""Module with a function that prints a matrix of integers."""
+
+
+def print_matrix_integer(matrix=[[]]):
+    """function that prints a matrix of integers."""
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][col]), end="")
+            if col == len(matrix[row]) - 1:
+                print()
+            else:
+                print(" ", end="")
