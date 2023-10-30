a5b8b283a2d5bd53f857d79589c284362dd3879b
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-30 13:10:35 +0300

    W291 trailing whitespace

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""function that inserts a line of text to a file, 
+"""function that inserts a line of text to a file,
 after each line containing a specific string"""
 
 
 def append_after(filename="", search_string="", new_string=""):
     """function that inserts a line of text to a file, after each line
-    containing a specific string 
+    containing a specific string
 
     Args:
         filename (str): The name of the file.
