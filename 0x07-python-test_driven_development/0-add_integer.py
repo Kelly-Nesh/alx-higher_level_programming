99af05d946359a1122f5e763e8f36a3abe1a3ff9
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-30 12:32:32 +0300

    added tests and made executable

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" 
+"""
 Adds integers
     a and b must be integers or floats, otherwise raise a TypeError exception
     with the message a must be an integer or b must be an integer
