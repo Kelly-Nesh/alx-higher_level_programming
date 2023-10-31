7ee390b57c3bb7db2cafd25eabf3eb4e3a4fd020
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-31 13:08:35 +0300

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -4,37 +4,42 @@
 
 class Rectangle:
     """A rectangle class representation."""
-
     def __init__(self, width=0, height=0):
         """Initialize rectangle properties.
 
         Args:
-            width (int). The width of the rectangle.
-            height (int). The height of the rectangle.
+            width (int). The width of the rectangle
+            height (int). The height of the rectangle
         """
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     @property
     def width(self):
-        """Get or Set the width of the current rectangle."""
+        """Get the width of the current rectangle."""
         return self.__width
 
     @width.setter
-    def width(self, value):
-        if not isinstance(value, (int)):
+    def width(self, width):
+        """Set the width of the current rectangle
+            depending on type and value
+        """
+        if not isinstance(width, (int)):
             raise TypeError('width must be an integer')
-        elif value < 0:
+        elif width < 0:
             raise ValueError('width must be >= 0')
-        self.__width = value
+        self.__width = width
 
     @property
     def height(self):
-        """Get or set the height of the current rectangle."""
+        """Get the height of the current rectangle."""
         return self.__height
 
     @height.setter
     def height(self, height):
+        """Set the height of the current rectangle
+            depending on type and value
+        """
         if not isinstance(height, (int)):
             raise TypeError('height must be an integer')
         elif height < 0:
@@ -47,8 +52,8 @@ class Rectangle:
 
     def perimeter(self):
         """Returns rectangle perimeter."""
-        if self.__width == 0 and self.__height == 0:
-            return (0)
+        if self.__width == 0 or self.__height == 0:
+            return 0
         return (self.__width + self.__height) * 2
 
     def __str__(self):
