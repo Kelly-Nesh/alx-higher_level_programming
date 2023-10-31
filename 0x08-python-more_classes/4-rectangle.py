52997c0ea76bc9674f8d82add2652b3d7e4c46a1
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-31 13:14:44 +0300

    4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         """Set the width of the current rectangle
             depending on type and value
         """
-        if not isinstance(width, (int)):
+        if not isinstance(width, int):
             raise TypeError('width must be an integer')
         elif width < 0:
             raise ValueError('width must be >= 0')
@@ -40,7 +40,7 @@ class Rectangle:
         """Set the height of the current rectangle
             depending on type and value
         """
-        if not isinstance(height, (int)):
+        if not isinstance(height, int):
             raise TypeError('height must be an integer')
         elif height < 0:
             raise ValueError('height must be >= 0')
