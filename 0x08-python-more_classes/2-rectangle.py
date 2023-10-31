6599a1e2c692db07fdd3804330eb500fde075495
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-10-31 13:03:26 +0300

    perimeteer condition

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -52,6 +52,6 @@ class Rectangle:
 
     def perimeter(self):
         """Returns rectangle perimeter."""
-        if self.__width == 0 and self.__height == 0:
+        if self.__width == 0 or self.__height == 0:
             return 0
         return (self.__width + self.__height) * 2
