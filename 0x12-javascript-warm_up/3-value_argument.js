17dcbc5b7c6f68a9055753015b88584ebd33adc2
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-13 20:12:27 +0300

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
-if (argv.length < 3) {
-  console.log('No argument');
-} else {
-  console.log(argv[2]);
-}
+let v = argv[2];
+v ? console.log(v) : console.log('No argument');
