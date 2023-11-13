745098084889bade1e0693aa7382e68a2684ed05
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-13 22:00:54 +0300

    fixed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-let v = argv[2];
-v ? console.log(v) : console.log('No argument');
+const argv = process.argv;
+
+argv[2] ? console.log(argv[2]) : console.log('No argument');
