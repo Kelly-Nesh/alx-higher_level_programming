ee6f83e05afa0b0211e7295b8c4e26b1b8f0c641
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-14 05:47:36 +0300

    3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,4 +1,2 @@
 #!/usr/bin/node
-const argv = process.argv;
-
-argv[2] ? console.log(argv[2]) : console.log('No argument');
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
