03a60c5ae7b2f665c0baacfc44b6176e5fbb84e5
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-14 10:11:17 +0300

    101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 
-const {dict} = require("./101-data.js");
-set = new Set(Object.values(dict));
-let newdict = {}
-for (let i of set) {
-	let list = [];
-	for (let k of Object.keys(dict)) {
-		if (dict[k] === i) {
-		list.push(`${k}`)
-		}
-	newdict[i.toString()] = list;
-	}
+const { dict } = require('./101-data.js');
+const set = new Set(Object.values(dict));
+const newdict = {};
+for (const i of set) {
+  const list = [];
+  for (const k of Object.keys(dict)) {
+    if (dict[k] === i) {
+      list.push(`${k}`);
+    }
+    newdict[i.toString()] = list;
+  }
 }
 console.log(newdict);
