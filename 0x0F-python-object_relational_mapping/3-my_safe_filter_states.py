3d745a09118a052178b69250c1f6e0c57a2e8c72
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-19 17:12:51 +0300

    3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb as mysql
+import sys
+
+if __name__ == "__main__":
+    if '\'' in sys.argv[4]:
+        sys.argv[4] = sys.argv[4].split('\'')[0]
+    db = mysql.connect(host='localhost', user=sys.argv[1],
+                       password=sys.argv[2], port=3306, db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT id, name FROM states WHERE name
+                LIKE BINARY '{}' ORDER BY(id) ASC""".format(sys.argv[4]))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
