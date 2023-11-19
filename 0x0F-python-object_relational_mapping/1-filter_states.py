f70d0ea2d3c5370e85ca5dab152fa74716094e3e
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-19 16:42:34 +0300

    case sensitive - like binary

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
 
     cur = db.cursor()
     cur.execute("""SELECT id, name FROM states
-                WHERE name LIKE 'N%' ORDER BY(id) ASC""")
+                WHERE name LIKE BINARY 'N%' ORDER BY(id) ASC""")
     states = cur.fetchall()
     for state in states:
         print(state)
