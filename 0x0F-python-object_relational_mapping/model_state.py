40b22cb4e973d1d39b8dbc06f944bd44b862c4c3
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-19 19:45:23 +0300

    model_state.py

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+contains the class definition of a State
+    inherits from Base Tips
+    links to the MySQL table states
+"""
+from sqlalchemy import Column, Integer, String, Sequence, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import sys
+
+Base = declarative_base()
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, Sequence('states_id_seq'), primary_key=True)
+    name = Column(String(128), nullable=False)
