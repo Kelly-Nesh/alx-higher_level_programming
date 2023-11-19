1c04fad0a61454cf9c06307515875a73dedb6a92
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-19 19:57:17 +0300

    not executed when imported

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,11 +10,10 @@ import sys
 
 Base = declarative_base()
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        'root', 'root', 'hbtn_0e_6_usa', pool_pre_ping=True))
-    Base.metadata.create_all(engine)
-
     class State(Base):
         __tablename__ = "states"
         id = Column(Integer, Sequence('states_id_seq'), primary_key=True)
         name = Column(String(128), nullable=False)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        'root', 'root', 'hbtn_0e_6_usa', pool_pre_ping=True))
+    Base.metadata.create_all(engine)
