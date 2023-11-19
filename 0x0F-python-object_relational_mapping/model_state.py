8d32acc86d70894b302684ab229463323738607a
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-19 19:58:35 +0300

    not executed when imported

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -9,11 +9,15 @@ from sqlalchemy.ext.declarative import declarative_base
 import sys
 
 Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = "states"
+    id = Column(Integer, Sequence('states_id_seq'), primary_key=True)
+    name = Column(String(128), nullable=False)
+
+
 if __name__ == "__main__":
-    class State(Base):
-        __tablename__ = "states"
-        id = Column(Integer, Sequence('states_id_seq'), primary_key=True)
-        name = Column(String(128), nullable=False)
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         'root', 'root', 'hbtn_0e_6_usa', pool_pre_ping=True))
     Base.metadata.create_all(engine)
