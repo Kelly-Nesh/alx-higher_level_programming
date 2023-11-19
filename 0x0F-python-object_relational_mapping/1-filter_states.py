ebb812dac3b3750092f90d9f1924f24cc51dcd92
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-19 16:36:04 +0300

    1-filter_states.py

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""script that lists all states from the database hbtn_0e_0_usa
+"""
+import MySQLdb as mysql
+import sys
+
+if __name__ == "__main__":
+    db = mysql.connect(host='localhost', user=sys.argv[1],
+                       password=sys.argv[2], port=3306, db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("""SELECT id, name FROM states
+                WHERE name LIKE 'N%' ORDER BY(id) ASC""")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
