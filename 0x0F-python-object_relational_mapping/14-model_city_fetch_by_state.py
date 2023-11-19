6d1b2b8d42fc2c41d0391f618607efcd29edbb12
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-11-19 22:18:30 +0300

    fixed query

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -14,8 +14,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    query = select(State.name, City.id, City.name).order_by(
-        City.id).join(State, State.id == City.id)
+    query = select(State.name, City.id, City.name).select_from(City).join(State, City.state_id==State.id)
     states = session.execute(query)
     for instance in states:
         print("{}: ({}) {}".format(instance[0], instance[1], instance[2]))
