3c8771d312b1dafd70c02a470871b954428aacce
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-12-04 12:58:27 +0300

    added documentation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,6 +2,10 @@
 """Write a function that finds a peak in a list of unsorted integers."""
 
 def find_peak(list_of_integers):
+	"""finds a peak in a list of unsorted integers.
+	Args:
+		list_of_integers: (list) list of integers
+	"""
     list_of_integers.sort()
     if list_of_integers:
         return list_of_integers[-1]
