2fd7824c36afc9ccf1a35fc69ec53bc50d8bd90d
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-12-04 11:44:01 +0300

    3lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL, and displays 
-# the size body of the response
+# displays content length 
 curl -sI "$1" | grep -E "Content-Length" | cut -b 17-
