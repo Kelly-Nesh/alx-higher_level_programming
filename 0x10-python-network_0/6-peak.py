f8ebcd69cf08f222e2b1a19da9f53e50ffb458ac
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-12-04 13:12:00 +0300

    documentation autopep

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """Write a function that finds a peak in a list of unsorted integers."""
 
+
 def find_peak(list_of_integers):
-	"""finds a peak in a list of unsorted integers.
-	Args:
-		list_of_integers: (list) list of integers
-	"""
+    """finds a peak in a list of unsorted integers.
+    Args:
+        list_of_integers: (list) list of integers
+    """
+    if not list_of_integers:
+        return
     list_of_integers.sort()
-    if list_of_integers:
-        return list_of_integers[-1]
-    return
+    return list_of_integers[-1]
