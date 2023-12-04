1815465870ad3a96f5d4f92f454d1102d6534147
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-12-04 11:54:03 +0300

    redirect last status code

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # displays body of response if 200 status
-status="$(curl -sI -L "$1" | grep HTTP | cut -b 10-13)"; status="$(echo $status | cut -b 5-)"; if [ $status -eq 200 ]; then curl -s -L "$1";fi
+if [ $(curl -sI -L "$1" | grep HTTP | cut -b 10-13 | tail -n 1) -eq 200 ]; then curl -s -L "$1";fi
