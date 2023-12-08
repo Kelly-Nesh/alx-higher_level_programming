dbc44b218a20991b08e9dd9a01c34c12c5f6b3a9
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-12-09 00:07:11 +0300

    0-hbtn_status.py

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script that fetches https://alx-intranet.hbtn.io/status"""
+import urllib.request as rq
+
+
+if __name__ == "__main__":
+    req = rq.Request("https://alx-intranet.hbtn.io/status")
+    with rq.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
