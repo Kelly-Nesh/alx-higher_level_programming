5548eaa542781feb03527d75594b5c1aaec10b4d
Kelly-Nesh <cs.kelly.nesh@gmail.com>
2023-12-09 00:25:08 +0300

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Sends a POST request to http://0.0.0.0:5000/search_user with a given letter.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    pl = {"q": letter}
+
+    r = requests.post("http://0.0.0.0:5000/search_user", data=pl)
+    try:
+        res = r.json()
+        if res == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(res.get("id"), res.get("name")))
+    except ValueError:
+        print("Not a valid JSON")
